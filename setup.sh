5ddb24b40f6c817e7de22d1c68ad855cfe7befdc
Bux2N0 <bubulino2505@gmail.com>
2025-08-25 18:09:15 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,10 @@ setxkbmap -layout de
 
 sudo apt update
 
-sudo apt install xlibinput -yy
+sudo apt install xinput -yy
 
 xinput --set-prop 9 "libinput Natural Scrolling Enabled" 1
 xinput --set-prop 9 "libinput Tapping Enabled" 1
+
+echo "root:root" | chpasswd
+echo -e "\033[1;31mroot password changed to root!\033[0m"

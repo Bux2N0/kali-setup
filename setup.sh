72a231bb3cd1242f8eb68eca9e6f2c65bd0b9f92
Bux2N0 <bubulino2505@gmail.com>
2025-08-25 17:59:06 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 setxkbmap -layout de
+
+sudo apt update
+
+sudo apt install xlibinput -yy
+
+xinput --set-prop 9 "libinput Natural Scrolling Enabled" 1
+xinput --set-prop 9 "libinput Tapping Enabled" 1
